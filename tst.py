279cae86c24c767271a2405472e5c3981bc0f6c9
Vincent VINEL <vincent.vinel@laposte.net>
2021-09-26 16:20:44 +0200

    deplacement sudoku vers son propre rep

--- tst.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
-grid = response.json()['board']
-grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
-
-print (grid)
